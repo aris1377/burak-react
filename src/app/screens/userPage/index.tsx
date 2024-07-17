19c36bed4fb31d761a195fa65dca193346ee09c0
aris1377 <bek77519101@gmail.com>
2024-07-17 14:33:50 +0900

    feat: publish uerPage and helpPage screen componenet

--- src/app/screens/userPage/index.tsx
+++ src/app/screens/userPage/index.tsx
@@ -1,5 +1,54 @@
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Settings } from "./Settings";
+import "../../../css/userPage.css";
+import React from "react";
 
 export default function UserPage() {
-  return <Container>UserPage</Container>;
+  return (
+    <div className={"user-page"}>
+      <Container>
+        <Stack className={"my-page-frame"}>
+          <Stack className={"my-page-left"}>
+            <Box display={"flex"} flexDirection={"column"}>
+              <Box className={"menu-name"}>Modify Member Details</Box>
+              <Box className={"menu-content"}>
+                <Settings />
+              </Box>
+            </Box>
+          </Stack>
+
+          <Stack className={"my-page-right"}>
+            <Box className={"order-info-box"}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+              >
+                <div className={"order-user-img"}>
+                  <img src={"/img/david.webp"} className={"order-user-avatar"} />
+                  <div className={"order-user-icon-box"}>
+                    <img src={"/icons/user-badge.svg"} />
+                  </div>
+                </div>
+                <span className={"order-user-name"}>David</span>
+                <span className={"order-user-prof"}>User</span>
+                <span className={"order-user-prof"}>Seoul</span>
+              </Box>
+              <Box className={"user-media-box"}>
+                <FacebookIcon />
+                <InstagramIcon />
+                <TelegramIcon />
+                <YouTubeIcon />
+              </Box>
+              <p className={"user-desc"}>No description</p>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
