e6c2eda33dcdaab64342c282119c5d0e84f17058
aris1377 <bek77519101@gmail.com>
2024-07-28 12:50:20 +0900

    feat: crete Test class components

---
+++ src/app/screens/Test.tsx
@@ -0,0 +1,48 @@
+//@ts-nocheck
+
+import React, { Component } from "react";
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
+    };
+  }
+  changeDetail = () => {
+    this.setState({
+      color: "blue",
+      brand: "Tesla",
+      model: "Model S",
+      year: 2023,
+    });
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  componentDidUpdate() {}
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          Color: {this.state.color} - Model: {this.state.model}from{" "}
+          {this.state.year}.
+        </p>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Test;
