79267824ceb12a6ffeb977ac9ac5a7a5d382f66e
aris1377 <bek77519101@gmail.com>
2024-08-08 12:34:41 +0900

    feat: develop Orderspage redux config and create selector in orders page

--- src/app/screens/ordersPage/selector.ts
+++ src/app/screens/ordersPage/selector.ts
@@ -1,7 +1,7 @@
 import { createSelector } from "reselect";
 import { AppRootState } from "../../../lib/types/screen";
 
-const selectOrdersPage = (state: AppRootState) => state.homePage;
+const selectOrdersPage = (state: AppRootState) => state.ordersPage;
 export const retrievePausedOrders = createSelector(
   selectOrdersPage,
   (OrdersPage) => OrdersPage.pausedOrders
